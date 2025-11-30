c131f0fbd8c2b3b273906e244d6475b65c0577cb
zotel1 <zotelsigel@gmail.com>
2025-11-29 22:53:38 -0300

    agregamos la configuracion de firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp, getApps, FirebaseApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+let app: FirebaseApp;
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+};
+
+// Evitar inicializar Firebase más de una vez
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
+}
+
+export const db = getFirestore(app);
